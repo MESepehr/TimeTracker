2cac68e8392d0c4e4dad067c20e149d3878d1e6e
Mohammad Ebrahim Sepehr <ebrahimsepehr@gmail.com>
2018-04-20 12:15:26 +0430

    axios

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import StopWatch from './components/stopwatch/StopWatch'
+import StopWatch from './components/stopwatch/StopWatch';
+import axios from 'axios';
 
 
 export default class App extends React.Component
@@ -12,6 +13,42 @@ export default class App extends React.Component
             isCounting:false
         }
         this.stopWatchComponent = null ;
+    }
+
+    componentDidMount()
+    {
+        alert("Call teh web service");
+        //axios.get('https://jsonplaceholder.typicode.com/users')
+        var path=require('path');
+        var lib=path.join(path.dirname(require.resolve('axios')),'lib/adapters/http');
+        var http=require(lib);
+        axios.get('http://127.0.0.1:8000/api/getLastOpenedDuration',{
+            adapter: http,
+            headers: {
+                Authorization: "Basic YWRtaW46bHVveGlueGlhbjkx"
+            }
+        })
+        .then(this.lastSavedTimeRetuned.bind(this)).catch(
+            this.connectionError
+        );
+        /*axios.get('http://127.0.0.1:8000/api/getLastOpenedDuration')
+        .then(
+            this.lastSavedTimeRetuned.bind(this)
+        ).catch(
+            this.connectionError
+        );*/
+    }
+
+    /**{"id":1,"description":null,"duration":"2000","submitdate":null,"submitdone":false} */
+    lastSavedTimeRetuned(data)
+    {
+        alert("Data backed : "+data)
+    }
+
+    /**Show the connectino error popUp */
+    connectionError(error)
+    {
+        alert("Connectin Error : "+error);
     }
 
    toggleStopWatch()
